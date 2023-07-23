03f6a5ae5c8aefb10b77b4b3aef7aa92cc8cda6b
A Dhruva Krishnama Raju <48171972+DeeKay02@users.noreply.github.com>
2023-07-24 00:32:53 +0530

    Add Featured in row 2 and complete with alignment

--- src/components/Featured/index.js
+++ src/components/Featured/index.js
@@ -4,21 +4,42 @@ import Frame4 from '../../assets/Featured r1 1.png'
 import Frame3 from '../../assets/Featured r1 2.png'
 import Frame2 from '../../assets/Featured r1 3.png'
 import Frame1 from '../../assets/Featured r1 4.png'
+import Frame5 from '../../assets/Featured r2 5.png'
+import Frame6 from '../../assets/Featured r2 4.png'
+import Frame7 from '../../assets/Featured r2 3.png'
+import Frame8 from '../../assets/Featured r2 2.png'
+import Frame9 from '../../assets/Featured r2 1.png'
 
 const Featured = () => {
 
-  const images = [Frame1, Frame2, Frame3, Frame4]
-  const [currentIndex, setCurrentIndex] = useState(0);
+  const images1 = [Frame1, Frame2, Frame3, Frame4]
+  const [currentIndex1, setCurrentIndex1] = useState(0);
 
   useEffect(() => {
     const intervalId = setInterval(() => {
-      if(currentIndex === images.length - 1) {
-        setCurrentIndex(0);
+      if(currentIndex1 === images1.length - 1) {
+        setCurrentIndex1(0);
       } 
       else {
-        setCurrentIndex(currentIndex + 1);
+        setCurrentIndex1(currentIndex1 + 1);
       }
-    }, 2000)
+    }, 1000)
+    
+    return () => clearInterval(intervalId);
+  })
+
+  const images2 = [Frame5, Frame6, Frame7, Frame8, Frame9]
+  const [currentIndex2, setCurrentIndex2] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      if(currentIndex2 === images2.length - 1) {
+        setCurrentIndex2(0);
+      } 
+      else {
+        setCurrentIndex2(currentIndex2 + 1);
+      }
+    }, 1000)
     
     return () => clearInterval(intervalId);
   })
@@ -32,6 +53,7 @@ const Featured = () => {
       height: '100vh',
       width: '100%',
       backgroundRepeat: 'no-repeat',
+      overflow: 'hidden',
     }}>
       <h1 style={{
         color: 'white',
@@ -41,8 +63,10 @@ const Featured = () => {
         textTransform: 'uppercase',
         marginTop: '40px',
       }}>Featured In</h1>
-
-      <img src={images[currentIndex]} alt='frame' style={{width: '90%'}} />
+      <div style={{height: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'space-around', alignItems: 'center'}}>
+        <img src={images1[currentIndex1]} alt='frame' style={{width: '90%'}} />
+        <img src={images2[currentIndex2]} alt='frame' style={{width: '90%'}} />
+      </div>
     </div>
   )
 }
